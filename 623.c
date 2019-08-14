d8c9eb46dfa1d1314eaaf095cd450477ee21745d
Seheon Yu <tpgjsdl2@gmail.com>
2019-08-14 21:32:56 +0900

    pointer index

--- 623.c
+++ 623.c
@@ -5,9 +5,9 @@ int main()
     double value[5], *pointer = value;
 
     for (int i = 0; i < 5; i++)
-        scanf("%lf", pointer + i);
+        scanf("%lf", &pointer[i]);
 
     for (int i = 0; i < 5; i++)
-        printf("%.1lf ", *(pointer + i));
+        printf("%.1lf ", pointer[i]);
     printf("\n");
 }
